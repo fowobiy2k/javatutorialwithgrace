7772e3ed6b19a2b8e2b4725a3fc96df38d0ad6b6
Oluwatosin <fowobiy2k@yahoo.com>
2025-02-16 09:24:17 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.math.BigDecimal;
+
+public class Main {
+
+    static String GLOBAL_VALUE = "Union bank";
+
+    public static void main(String[] args) {
+        String MESSAGE = "hello world";
+        char x = 'a';
+        boolean done = false;
+        int age = 1288811114;
+        double cost = -23.99999;
+        long size = 528881111;
+
+        System.out.println(MESSAGE);
+        System.out.println(GLOBAL_VALUE);
+        System.out.println( "Age in main= " + age);
+        System.out.println("Age in getAge method= " + getAge());
+
+    }
+
+    public static int getAge() {
+        String greeting;
+
+        greeting  = "Hello, madam";
+        int age = 4;
+//        System.out.println(age);
+        System.out.println(GLOBAL_VALUE + "x2");
+
+        Messenger messenger = new Messenger();
+        System.out.println("Colour is: " + messenger.shape);
+
+        System.out.println("Colour is: " + Messenger.colour);
+
+
+        return age;
+    }
+}
---
+++ src/Messenger.java
@@ -0,0 +1,4 @@
+public class Messenger {
+    public static String colour = "magenta";
+    public String shape = "rhombus";
+}
