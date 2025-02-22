5d666da205523348701014e0af150079b670af10
Oluwatosin <fowobiy2k@yahoo.com>
2025-02-22 12:35:47 +0100

    added OperatorsDemo.java
    this class show how to perform operations in java

---
+++ src/OperatorsDemo.java
@@ -0,0 +1,25 @@
+public class OperatorsDemo {
+
+    public static void main(String[] args) {
+        int sum = 18 + 57;
+        System.out.println("The sum is: " + sum);
+
+        int diff = 92 - 20;
+        System.out.println("The difference is: " + diff);
+
+        int prod = 12 * 7;
+        System.out.println("The product is: " + prod);
+
+        int quo = 14 / 5;       //Integer division
+        System.out.println("The quotient is: " + quo);
+
+        int rem = 14 % 5;       //Remainder division (Modulo)
+        System.out.println("The remainder is: " + rem);
+
+        double cost = 14 / 5;
+        System.out.println("The quotient is: " + cost);
+
+        double cost2 = 2.5 * 3;
+        System.out.println("The product is: " + cost2);
+    }
+}
